d49c041cfee170c42a4fbb398b31a4d4123b9c8f
koishi-x <sjtu-xzq@sjtu.edu.cn>
2023-07-10 15:46:14 +0800

    Book2-Image8

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -30,11 +30,31 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        let i = ((4.0 * p.x) as i32 & 255) as usize;
-        let j = ((4.0 * p.y) as i32 & 255) as usize;
-        let k = ((4.0 * p.z) as i32 & 255) as usize;
+        // let i = ((4.0 * p.x) as i32 & 255) as usize;
+        // let j = ((4.0 * p.y) as i32 & 255) as usize;
+        // let k = ((4.0 * p.z) as i32 & 255) as usize;
 
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        // self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
+
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for (di, ci) in c.iter_mut().enumerate() {
+            for (dj, cij) in ci.iter_mut().enumerate() {
+                for (dk, cijk) in cij.iter_mut().enumerate() {
+                    *cijk = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]];
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
     }
 
     fn perlin_generate_perm() -> Vec<usize> {
@@ -56,5 +76,21 @@ impl Perlin {
             let target = random_int(0, i as i32) as usize;
             p.swap(i, target);
         }
+    }
+
+    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+
+        for (i, ci) in c.iter().enumerate() {
+            for (j, cij) in ci.iter().enumerate() {
+                for (k, cijk) in cij.iter().enumerate() {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * cijk;
+                }
+            }
+        }
+        accum
     }
 }
